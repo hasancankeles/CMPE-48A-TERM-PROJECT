792f0038fe7b45f868427cc734838d012a717afa
Barış Öksüz <baris.oksuz@codeway.co>
2025-12-14 17:05:00 +0300

    email-system-implemented

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import include, path
 
 
@@ -33,6 +33,8 @@ from .views import (
     badges_callback,
     UserBadgesView,
     recalculate_badges,
+    # Custom login view with email notification
+    CustomTokenObtainPairView,
 )
 
 
@@ -49,7 +51,7 @@ urlpatterns = [
     path("", UserListView.as_view(), name="user-list"),
     path("create/", CreateUserView.as_view(), name="create-user"),
     path("update/", UpdateUserView.as_view(), name="update-user"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/logout/", LogoutView.as_view(), name="token_logout"),
     path("change-password/", ChangePasswordView.as_view(), name="change-password"),
